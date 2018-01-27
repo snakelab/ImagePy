f9da776a48372d95e760ae02477fd5aa7fa62f94
hartmut <hartmut.koenig@uza.de>
2018-01-27 22:08:04 +0100

    fixed exifread on error

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ImagePy
 Move your images around
+
+## Used Bibs
+ https://github.com/ianare/exif-py
+
+## Install
+pip install -r requirements.txt
+or
+python3 -m pip install exifread argparse
+
+## Usage
+1. Change target_dir
+2. Run this command (to be ready for filenames with spaces):
+IFS='
+'&&set -f
+for file in $(find ~/Downloads)
+do
+./imagepy.py -f $file
+done
--- imagepy.py
+++ imagepy.py
@@ -1,64 +1,78 @@
-#!/bin/python
+#!/usr/bin/env python3
 import os
 import sys
 import exifread
 import datetime
+import time
 import re
+import argparse
+import shutil
 
-#try:
+parser = argparse.ArgumentParser(description='Process some images.')
+parser.add_argument('-f','--file', help='Image to work with?', required=True)
+
+args = parser.parse_args()
+#print(args)
+
+#-- Here happens the magic with existing/similar pictures
 from safeutil import move, copyfile
-#except ImportError:
-#    from shutil import move, copyfile
 
-target_dir="/volume1/photo/photosync"
-file=sys.argv[1]
+#-- should be the dir with the Date-Structure
+target_dir="/Volumes/photo/photosync"
 
-if str(os.path.basename(file)) == '.': 
-    sys.exit() 
-if str(os.path.basename(file)) == '..': 
+file=args.file
+
+if str(os.path.basename(file)) == '.':
+    sys.exit()
+if str(os.path.basename(file)) == '..':
     sys.exit()
 if os.path.isdir(file):
     sys.exit()
 # Open image file for reading (binary mode)
 f = open(file, 'rb')
-#print os.path.basename(file)
+
 # Return Exif tags
-tags = exifread.process_file(f)
+if file.lower().endswith(('.dng')):
+    tags = None
+else:
+    tags = exifread.process_file(f,stop_tag='DateTimeOriginal',details=False,strict=True)
+#for tag in tags.keys():
+#    if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
+#        print("Key: %s, value %s" % (tag, tags[tag]))
+#print(tags)
+
 try:
-    if tags.get('EXIF DateTimeOriginal'):
-        #for tag in tags.keys():
-        #    if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
-        #        print "Key: %s, value %s" % (tag, tags[tag])
-        print "EXIF"
+    if tags is not None and tags.get('EXIF DateTimeOriginal'):
+        print("EXIF")
         date=datetime.datetime.strptime(str(tags['EXIF DateTimeOriginal']), "%Y:%m:%d %H:%M:%S")
+
         dir="%s/%4d/%02d" % (target_dir, date.year, date.month)
         target="%s/%s" % (str(dir), str(os.path.basename(file).lower()))
     elif re.search(r"IMG_(\d{4})(\d{2})(\d{2}).*", os.path.basename(file), flags=re.I):
-        print "NAME"
+        print("NAME")
         parts=re.search(r"IMG_(\d{4})(\d{2})(\d{2}).*", os.path.basename(file), flags=re.I)
+
         dir="%s/%4d/%02d" % (target_dir, int(parts.group(1)), int(parts.group(2)))
     else:
-        print "OHNE"
-        dir="%s/0000/00" % (target_dir, int(parts.group(1)), int(parts.group(2)))
-        #try:
-        #    os.makedirs(dir)
-        #except OSError:
-        #    pass
-    #print file
-    target="%s/%s" % (str(dir), str(os.path.basename(file).lower()))
-except NameError:
-    created=0
+        print("OHNE")
+        parts=time.gmtime(os.path.getmtime(file))
 
-created=0
+        dir="%s/%4d/%02d" % (target_dir, int(parts.tm_year), int(parts.tm_mon))
+
+    target="%s/%s" % (str(dir), str(os.path.basename(file).replace(" ", "-").lower()))
+except NameError:
+    print("==>ExifRead Exception!")
+
+existing=0
 if file and target and dir:
-    #print target
     try:
         os.makedirs(dir)
     except OSError:
-        created=1
-    print 'Move file =>' + move(file,target) 
-    #move(file,target)
+        existing=1
+    #-- be aware of creation date
+    #print('Move file =>' + shutil.move(file,target))
+    #-- this function checks all and dont override existing files
+    move(file,target)
     if os.path.isfile(file):
-        print "Datei ist immernoch vorhanden!"
+        print("Image is still existing!")
         exit
-
